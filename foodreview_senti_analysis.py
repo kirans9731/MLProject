10d26adf3cc003705582f43c6f5cb73852b63d52
Kiran S <kirans@Kirans-MacBook-Pro.local>
2017-04-05 21:12:19 -0500

    added preprocessing steps

--- foodreview_senti_analysis.py
+++ foodreview_senti_analysis.py
@@ -1,15 +1,19 @@
 import pandas
 import numpy 
+import time
 from sklearn.model_selection import train_test_split
 
 from preprocessor import preprocess
 from classifier import kfold_cross_validate
 
 def main():
+
+	start_time = time.time()
+
 	data_path = 'data/amazon-fine-foods/Reviews.csv'
 	reviews_df = pandas.read_csv(data_path)
 
-	#reviews_df = reviews_df.head(1000)
+	#reviews_df = reviews_df.head(10)
 
 	train, test = train_test_split(reviews_df, test_size = 0.2)
 
@@ -19,6 +23,8 @@ def main():
 	kfold_res = kfold_cross_validate(train, [], k=10)
 
 	print(kfold_res)
+
+	print("Total time take in seconds = %s"% (time.time() - start_time))
 
 if __name__ == '__main__':
 	main()
--- preprocessor.py
+++ preprocessor.py
@@ -1,17 +1,41 @@
 import nltk
 
 from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
 
 def preprocess(reviews_df):
 
+	#print(reviews_df['Text'])
+
 	#lower
 	reviews_df.Text = reviews_df.Text.str.lower() 
+	reviews_df.Summary = reviews_df.Summary.str.lower()
 
-	# Remove Stopwords
-	#stop_words = stopwords.words('english')
-	#reviews_df['Text'] = reviews_df['Text'].apply(lambda x: [item for item in x if item not in stop_words])
+	#add summary to text
+	reviews_df['Text'] = reviews_df['Text'] + ' ' +reviews_df['Summary'].map(str)
+
+	#Remove Stopwords
+	stop_words = stopwords.words('english')
+	stop_words.remove('no');
+	stop_words.remove('nor');
+	stop_words.remove('not');
+	stop_words.remove('won');
+	reviews_df['Text'] = reviews_df['Text'].apply(lambda x: ' '.join([item for item in x.split(' ') if item not in stop_words]))
+
+	#Remove HTML Characters
+	reviews_df['Text'] = reviews_df['Text'].apply(lambda x: ' '.join(BeautifulSoup(x,"html.parser").findAll(text=True)))
+
+	#remove numbers
+	reviews_df['Text'] = reviews_df['Text'].apply(lambda x: ' '.join(word for word in x.split() if not any(char.isdigit() for char in word)))	
+
+	#remove punctuations
+	reviews_df['Text'] = reviews_df['Text'].apply(lambda x: ' '.join(word.strip('\'"&@:_!?,.()#+-~=/|*[];$<>') for word in x.split()))
 
 	# Add a class
 	reviews_df['Class'] = (reviews_df.Score > 3).astype(int)
+
+	#print(reviews_df['Text'])
+	reviews_df.to_csv('test.csv', sep=',')
 
 	return reviews_df
